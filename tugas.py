3eeca8d0ba86246b53a2b2dbed393c05c31117a0
gheraldeva <gheral.nandra11@gmail.com>
2024-04-04 10:18:19 +0800

    commit anggota 1

---
+++ tugas.py
@@ -0,0 +1,32 @@
+print ("===================================")
+# anggota 1
+nama = "Gheral Deva Bagus Archana"
+kelas = "X RPL 2"
+agama = "Hindu"
+alamat = "Karang Bayan, Lingsar"
+jenis_kelamin = "Laki Laki"
+umur = 17
+
+print("Nama : ",nama)
+print("Kelas : ",kelas)
+print("Agama : ",agama)
+print("Alamat : ",alamat)
+print("Jenis Kelamin : ",jenis_kelamin)
+print("Umur : ", umur)
+print ("===================================")
+
+# anggota 2
+nama = "Gheral Deva Bagus Archana"
+kelas = "X RPL 2"
+agama = "Hindu"
+alamat = "Karang Bayan, Lingsar"
+jenis_kelamin = "Laki Laki"
+umur = 16
+
+print("Nama : ",nama)
+print("Kelas : ",kelas)
+print("Agama : ",agama)
+print("Alamat : ",alamat)
+print("Jenis Kelamin : ",jenis_kelamin)
+print("Umur : ", umur)
+print ("===================================")
